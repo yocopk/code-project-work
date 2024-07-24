ad3213709f211aacd8b307e7c9cf4759e771588c
yocopk <yocopk@gmail.com>
2024-07-24 16:56:18 +0200

    update db file and users

--- src/models/Order.ts
+++ src/models/Order.ts
@@ -12,15 +12,3 @@ export class ModelOrder {
     this.status = "pending";
   }
 }
-
-    primaryKey: string;
-    referenceKeyUser: string;
-    referenceKeyProduct: string;
-    status: string;
-    constructor(referenceKeyUser: string, referenceKeyProduct: string) {
-        this.primaryKey = Math.random().toString(36).slice(2);
-        this.referenceKeyUser = referenceKeyUser;
-        this.referenceKeyProduct = referenceKeyProduct;
-        this.status = 'pending';
-    }
-}
