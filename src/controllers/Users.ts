023eca945be7ce5292fe838e0f3d6943c62f1ff6
Samuele Salica <reiz@MBP-di-Samuele-2.homenet.telecomitalia.it>
2024-07-24 15:19:09 +0200

    AddModelandControllerCART

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,33 +1,33 @@
 import { ModelUser } from "../models/User";
 
 export class ControllerUsers {
-    private users: ModelUser[] = [];
-    private adminWhitelist: ModelUser[] = [];
-    
-    register(email: string, password: string){
-        const userFound = this.users.find((user) => {
-            return user.email === email;
-          });
+  private users: ModelUser[] = [];
+  private adminWhitelist: ModelUser[] = [];
 
-        if (userFound) return console.log('Utente già registrato.');
-        else {
-            const newUser = new ModelUser(email, email, password);
-            this.users = [...this.users, newUser];
-            return true;
-        }
+  register(email: string, password: string) {
+    const userFound = this.users.find((user) => {
+      return user.email === email;
+    });
+
+    if (userFound) return console.log("Utente già registrato.");
+    else {
+      const newUser = new ModelUser(email, email, password);
+      this.users = [...this.users, newUser];
+      return true;
     }
+  }
 
-    registerAdmin(email: string, password: string){
-        const adminFound = this.adminWhitelist.find((admin) => {
-            return admin.email === email;
-        })
+  registerAdmin(email: string, password: string) {
+    const adminFound = this.adminWhitelist.find((admin) => {
+      return admin.email === email;
+    });
 
-        if (adminFound) return console.log('Admin già registrato.');
-        else {
-            const newAdmin = new ModelUser(email, email, password);
-            newAdmin.isAdmin = true;
-            this.adminWhitelist = [...this.adminWhitelist, newAdmin];
-            return true;
-        }
+    if (adminFound) return console.log("Admin già registrato.");
+    else {
+      const newAdmin = new ModelUser(email, email, password);
+      newAdmin.isAdmin = true;
+      this.adminWhitelist = [...this.adminWhitelist, newAdmin];
+      return true;
     }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from 'express';
-import { createClient } from '@vercel/postgres';
-import { createServer } from 'http';
-import { config } from 'dotenv';
+import express, { Request, Response } from "express";
+import { createClient } from "@vercel/postgres";
+import { createServer } from "http";
+import { config } from "dotenv";
 
 config();
 
@@ -9,13 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const server = createServer(app);
 const client = createClient({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
 });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
-})
+});
 
 server.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
