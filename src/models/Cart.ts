d35bbbbd2186483a4134b1409f9676c86a11d302
yocopk <yocopk@gmail.com>
2024-07-24 17:34:36 +0200

    general fixes

--- src/models/Cart.ts
+++ src/models/Cart.ts
@@ -1,3 +1,6 @@
+import { ModelUser } from "./User";
+import { ModelProduct } from "./Product";
+import {v4 as uuidv4} from 'uuid';
 export class ModelCart {
   primaryKey: number;
   referenceKeyUser: string;
